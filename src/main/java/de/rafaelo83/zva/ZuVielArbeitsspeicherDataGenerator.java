0324d1ca0e5b3201cf47847b8e11d2df9d49d810
rafaelo83 <jankovicrafael2@gmail.com>
2025-01-03 13:43:31 +0100

    Initial commit

---
+++ src/main/java/de/rafaelo83/zva/ZuVielArbeitsspeicherDataGenerator.java
@@ -0,0 +1,11 @@
+package de.rafaelo83.zva;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ZuVielArbeitsspeicherDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
